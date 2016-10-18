ec31ed380916ee8ec1b2f23e9ce810fb5fe94e6b
aqqzzz <592710057@qq.com>
2016-10-18 21:45:53 +0800

    activity

--- SportsZone/js/scripts.js
+++ SportsZone/js/scripts.js
@@ -65,3 +65,24 @@
     });
 
 })(jQuery); // End of use strict
+
+/**
+ * 鼠标划过就展开子菜单，免得需要点击才能展开
+ */
+function dropdownOpen() {
+
+    var $dropdownLi = $('li.dropdown');
+
+    $dropdownLi.mouseover(function() {
+        $(this).addClass('open');
+    }).mouseout(function() {
+        $(this).removeClass('open');
+    });
+}
+
+$(document).ready(function () {
+    $(document).off('click.bs.dropdown.data-api');
+    dropdownOpen();
+})
+
+
